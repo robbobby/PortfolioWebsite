c3b1706e8ad4aebb51295d12056c7e4132369be9
robbobby <69080884+robbobby@users.noreply.github.com>
2021-10-04 16:47:01 +0100

    Home page and nav bar setup

---
+++ src/containers/Portfolio.js
@@ -0,0 +1,53 @@
+import "../components/css/nav.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+
+const Portfolio = () => {
+	return (
+		<>
+			<Router>
+
+				<div id="nav-bar">
+
+
+					<div id="nav-bar-left">
+						<li className="nav-bar-item-left">
+							<Link className="nav-bar-link"
+								to={{ pathname: "https://www.linkedin.com/in/robert-hollingworth/" }}
+								target="_blank" > LinkedIn </Link>
+						</li>
+						<li className="nav-bar-item-left">
+							<Link className="nav-bar-link"
+								to={{ pathname: "https://github.com/robbobby" }}
+								target="_blank" > GitHub </Link>
+						</li>
+					</div>
+
+
+					<div id="nav-bar-right">
+						<li className="nav-bar-item-right">
+							<Link className="nav-bar-link" to="/">
+								Home
+							</Link>
+						</li>
+						<li className="nav-bar-item-right">
+							<Link className="nav-bar-link" to="/about">
+								About
+							</Link>
+						</li>
+						<li className="nav-bar-item-right">
+							<Link className="nav-bar-link" to="/projects">
+								Projects
+							</Link>
+						</li>
+					</div>
+
+
+
+				</div>
+			</Router>
+		</>
+	);
+};
+
+export default Portfolio;
